8726e3e8e0c33c1b4034f3d5ef9e9f8fe2b15a76
oxleberry <oxleberry@gmail.com>
2018-06-13 15:54:26 -0700

    make data model

--- stance_django/stance/models.py
+++ stance_django/stance/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Stock(models.Model):
+    symbol = models.CharField(max_length=10)
+    users = models.ManyToManyField(User)
+
+    def __str__(self):
+        return self.symbol
